595155232cf85e897ae5c0f3a79b533b633579fb
Sunabha07 <sunabhapanda@gmail.com>
2024-02-06 01:31:34 +0530

    git add

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const express = require('express')
 const app = express() // all the functionalities of express stored in this app variable 
 // const port = 3000  // server port
 
+const gitData = {
+    Sunabha
+    panda bultu mamata saitkat 
+    jfhgkhn
+    jkjgfkj 
+    kdfjhfbnftbhkj 
+    ihbfghbvbn 
+    ijfhvuifh 
+    ijkbifdhjbvui 
+    kjmfbij 
+    jhfuhg 
+    bhjtuhu 
+     ijifghjbi 
+
+}
+
 app.get('/', (req, res) => {  // 
   res.send('Hello World!')
 })
@@ -16,6 +32,11 @@ app.get('/insta',(req,res) => {
 app.get('/linkedin',(req,res) => {
     res.send('hello Sunabha Panda in your linkedin account heheheheheheh')
 })
+
+app.get('github',(req,res) => {
+    res.json(gitData)
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
